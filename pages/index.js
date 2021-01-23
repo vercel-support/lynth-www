fd1b6343e9049fa469f70d7082f109d772c13e20
Mateusz Janota <m.janota@lynth.io>
2021-01-23 13:51:14 +0100

    Fixed bg image scaling

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,7 @@ export default function Home() {
       <Image
         src="/images/background.jpg"
         layout="fill"
+        objectFit="cover"
       />
 
       <div className="fixed top-0 left-0 flex flex-col items-center justify-center h-screen w-screen">
