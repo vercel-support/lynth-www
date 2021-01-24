eb515dc1f0dcc766046c02e5c96a46106b00821f
Mateusz Janota <m.janota@lynth.io>
2021-01-24 11:46:05 +0100

    Fixed mobile

--- pages/index.js
+++ pages/index.js
@@ -33,58 +33,60 @@ export default function Home() {
         objectPosition="center center"
       />
 
-      <div className="fixed top-0 left-0 flex flex-col items-center justify-center h-screen w-screen">
-        <Image
-          src="/images/logo/logo.svg"
-          width={200}
-          height={200}
-        />
-        <p className="text-black dark:text-white text-2xl tracking-widest">
-          Learn. Build. Innovate.
-        </p>
+      <div className="fixed overflow-auto lg:overflow-hidden top-0 left-0 flex flex-col items-center lg:justify-center h-screen w-screen">
+        <div className="flex flex-col items-center">
+          <Image
+            src="/images/logo/logo.svg"
+            width={200}
+            height={200}
+          />
+          <p className="text-black dark:text-white text-2xl tracking-widest">
+            Learn. Build. Innovate.
+          </p>
 
-        <div className="mx-auto">
-          <div className="w-full grid gap-6 grid-cols-3 mt-16">
+          <div className="mx-auto">
+            <div className="w-full grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-16">
 
-            <Link href='https://lynth.jetbrains.space'>
-              <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
-                <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
-                <p className="text-white text-4xl">Space &rarr;</p>
-                <p className="text-white text-md mt-2 font-light">Join to our jetBrains Space.</p>
+              <Link href='https://lynth.jetbrains.space'>
+                <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
+                  <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
+                  <p className="text-white text-4xl">Space &rarr;</p>
+                  <p className="text-white text-md mt-2 font-light">Join to our jetBrains Space.</p>
+                </a>
+              </Link>
+
+              <Link href='https://discord.gg/eXxsq67kKS'>
+                <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
+                  <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
+                  <p className="text-white text-4xl">Discord &rarr;</p>
+                  <p className="text-white text-md mt-2 font-light">Come and talk with us!</p>
+                </a>
+              </Link>
+
+              <Link href='https://lynth.statuspage.io'>
+                <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
+                  <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
+                  <p className="text-white text-4xl">Status &rarr;</p>
+                  <p className="text-white text-md mt-2 font-light">Check our infrastructure status.</p>
+                </a>
+              </Link>
+
+              <a className="border-2 rounded-md px-8 py-4 hover:border-red-400 cursor-pointer" target="_blank">
+                <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-red-100 text-black">Coming soon</p>
+                <p className="text-white text-4xl">Website &rarr;</p>
+                <p className="text-white text-md mt-2 font-light">Our website is under construction.</p>
               </a>
-            </Link>
 
-            <Link href='https://discord.gg/eXxsq67kKS'>
-              <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
-                <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
-                <p className="text-white text-4xl">Discord &rarr;</p>
-                <p className="text-white text-md mt-2 font-light">Come and talk with us!</p>
+              <a className="border-2 rounded-md px-8 py-4 hover:border-red-400 cursor-pointer">
+                <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-red-100 text-black">Coming soon</p>
+                <p className="text-white text-4xl">Workshop &rarr;</p>
+                <p className="text-white text-md mt-2 font-light">Want to learn? Click here!</p>
               </a>
-            </Link>
 
-            <Link href='https://lynth.statuspage.io'>
-              <a className="border-2 rounded-md px-8 py-4 hover:border-blue-400" target="_blank">
-                <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-green-100 text-black">Available</p>
-                <p className="text-white text-4xl">Status &rarr;</p>
-                <p className="text-white text-md mt-2 font-light">Check our infrastructure status.</p>
-              </a>
-            </Link>
-
-            <a className="border-2 rounded-md px-8 py-4 hover:border-red-400 cursor-pointer" target="_blank">
-              <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-red-100 text-black">Coming soon</p>
-              <p className="text-white text-4xl">Website &rarr;</p>
-              <p className="text-white text-md mt-2 font-light">Our website is under construction.</p>
-            </a>
-
-            <a className="border-2 rounded-md px-8 py-4 hover:border-red-400 cursor-pointer">
-              <p className="px-3 py-1 mb-4 inline-flex text-md leading-5 font-normal rounded-full bg-red-100 text-black">Coming soon</p>
-              <p className="text-white text-4xl">Workshop &rarr;</p>
-              <p className="text-white text-md mt-2 font-light">Want to learn? Click here!</p>
-            </a>
-
+            </div>
           </div>
-        </div>
 
+        </div>
       </div>
 
     </React.Fragment>
