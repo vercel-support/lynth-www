c5fa09cf843caf07be3a022994a5e25dfde28c99
Mateusz Janota <m.janota@lynth.io>
2021-02-03 18:43:06 +0100

    Reverted tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,37 @@ class MyDocument extends Document {
       <Html lang="en" style={{background: '#000000'}}>
         <Head>
           {isProduction && (
-            <Optimize optimize_id={process.env.optimize_id}/>
+            <React.Fragment>
+              <Optimize optimize_id={process.env.optimize_id}/>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    
+                    gtag('config', '${process.env.GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                    `,
+                }}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${process.env.gtm_id}');
+                  `,
+                }}
+              />
+            </React.Fragment>
           )}
           <link rel="preconnect" href="https://www.lynth.io/_next/static/" crossOrigin=""/>
           <link rel="preconnect" href="https://in.hotjar.com" crossOrigin=""/>
@@ -50,23 +80,6 @@ class MyDocument extends Document {
 
           {isProduction && (
             <React.Fragment>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    
-                    gtag('config', '${process.env.GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                    });
-                    `,
-                }}
-              />
               <Hotjar hjid={process.env.hjid} hjsv={process.env.hjsv}/>
             </React.Fragment>
           )}
