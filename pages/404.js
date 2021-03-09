db5a76c6d23bd342637fd87ab80510db1a087a63
Mateusz Janota <m.janota@lynth.io>
2021-03-09 21:09:33 +0100

    Changed 404 page style

--- pages/404.js
+++ pages/404.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react'
 import {NextSeo} from "next-seo"
 import Image from "next/image"
 import Link from 'next/link'
+import * as styles from '../css/Error.module.css'
 
-class Error extends Component {
+class NotFoundError extends Component {
   render() {
     return (
       <React.Fragment>
@@ -38,18 +39,9 @@ class Error extends Component {
         </div>
 
         <div className="relative flex flex-col items-center lg:justify-center h-full w-full lg:h-screen z-0">
-          <p className="text-white text-9xl font-medium">
-            404
-          </p>
-          <p className="text-white text-4xl mt-6">
-            We can't find that
-          </p>
-          <Link href='/'>
-            <a
-              className="mt-6 px-6 py-2 border-2 rounded-full text-white hover:bg-white hover:text-black transition-all">
-              Home page
-            </a>
-          </Link>
+          <span className="transform scale-150">
+            <p data-text={'Not found'} className={'text-white font-medium ' + styles.glitch}>Not found</p>
+          </span>
         </div>
 
       </React.Fragment>
@@ -57,4 +49,4 @@ class Error extends Component {
   }
 }
 
-export default Error;
+export default NotFoundError;
